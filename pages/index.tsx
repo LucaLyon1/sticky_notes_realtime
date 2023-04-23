775affaaffacb2b5d9ad28859cbb43925ab2a65f
Luca Savio <savioluca2@gmail.com>
2023-04-23 20:43:25 +0200

    channel form in progress

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,30 @@
 import { useStore } from "@/lib/store"
 import { Note, Channel } from "@/types"
+import { useState } from "react";
 
 export default function Home() {
-  const {notes, channels} = useStore({channelId:1})
+  const [channel, setChannel] = useState('')
 
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setChannel(e.currentTarget.value)
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log(channel);
+  }
   return (
     <main>
-      {JSON.stringify(notes)}
+      <div className="flex h-screen">
+        <div className="w-1/3 m-auto text-center align-middle bg-slate-300 py-24 rounded-lg">
+          <p className="mb-8">What channel do you want to go to ?</p>
+          <form onSubmit={handleSubmit}>
+            <input className="border-blue-400 border-2" type="number" name="channel" id="channel" value={channel} onChange={handleChange} />
+            <br />
+            <button className="bg-blue-400 rounded-md text-white p-2 mt-4 hover:bg-blue-300">Go to the channel</button>
+          </form>
+        </div>
+      </div>
     </main>
   )
 }
